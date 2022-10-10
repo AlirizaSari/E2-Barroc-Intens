bed3eb6c0704c3836c16ee0b4d0d85b39156ac81
AlirizaSari <72546588+AlirizaSari@users.noreply.github.com>
2022-10-10 14:11:41 +0200

    Homeform changed name

--- BarrocItems/HomeForm.cs
+++ BarrocItems/HomeForm.cs
@@ -1,8 +1,8 @@
 namespace BarrocItems
 {
-    public partial class Form1 : Form
+    public partial class HomeForm : Form
     {
-        public Form1()
+        public HomeForm()
         {
             InitializeComponent();
         }
