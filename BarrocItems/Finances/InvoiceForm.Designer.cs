14c2b305911a70636c82c84c2bd4c17b4a6624e3
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2022-10-11 11:51:19 +0200

    added buttons to InvoiceForm

--- BarrocItems/Finances/InvoiceForm.Designer.cs
+++ BarrocItems/Finances/InvoiceForm.Designer.cs
@@ -28,19 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblFactuur = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblCompanyName = new System.Windows.Forms.Label();
+            this.lblDateInvoice = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lblCompanyAdress = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // lblFactuur
+            // 
+            this.lblFactuur.AutoSize = true;
+            this.lblFactuur.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblFactuur.Location = new System.Drawing.Point(416, 59);
+            this.lblFactuur.Name = "lblFactuur";
+            this.lblFactuur.Size = new System.Drawing.Size(99, 27);
+            this.lblFactuur.TabIndex = 0;
+            this.lblFactuur.Text = "Factuur";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(207, 152);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(250, 27);
+            this.textBox1.TabIndex = 1;
+            // 
+            // lblCompanyName
+            // 
+            this.lblCompanyName.AutoSize = true;
+            this.lblCompanyName.Location = new System.Drawing.Point(33, 155);
+            this.lblCompanyName.Name = "lblCompanyName";
+            this.lblCompanyName.Size = new System.Drawing.Size(149, 20);
+            this.lblCompanyName.TabIndex = 2;
+            this.lblCompanyName.Text = "Naam van het bedrijf";
+            // 
+            // lblDateInvoice
+            // 
+            this.lblDateInvoice.AutoSize = true;
+            this.lblDateInvoice.Location = new System.Drawing.Point(33, 207);
+            this.lblDateInvoice.Name = "lblDateInvoice";
+            this.lblDateInvoice.Size = new System.Drawing.Size(54, 20);
+            this.lblDateInvoice.TabIndex = 4;
+            this.lblDateInvoice.Text = "Datum";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(207, 202);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(250, 27);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // lblCompanyAdress
+            // 
+            this.lblCompanyAdress.AutoSize = true;
+            this.lblCompanyAdress.Location = new System.Drawing.Point(33, 255);
+            this.lblCompanyAdress.Name = "lblCompanyAdress";
+            this.lblCompanyAdress.Size = new System.Drawing.Size(147, 20);
+            this.lblCompanyAdress.TabIndex = 7;
+            this.lblCompanyAdress.Text = "Adres van het bedrijf";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(207, 252);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(250, 27);
+            this.textBox2.TabIndex = 6;
             // 
             // InvoiceForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(914, 600);
+            this.Controls.Add(this.lblCompanyAdress);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lblDateInvoice);
+            this.Controls.Add(this.lblCompanyName);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblFactuur);
             this.Name = "InvoiceForm";
             this.Text = "InvoiceForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label lblFactuur;
+        private TextBox textBox1;
+        private Label lblCompanyName;
+        private Label lblDateInvoice;
+        private DateTimePicker dateTimePicker1;
+        private Label lblCompanyAdress;
+        private TextBox textBox2;
     }
 }
--- BarrocItems/HomeForm.cs
+++ BarrocItems/HomeForm.cs
@@ -14,6 +14,11 @@ namespace BarrocItems
 
         }
 
+        /// <summary>
+        /// Button to go to the invoiceMenu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnPurchases_Click(object sender, EventArgs e)
         {
             InvoiceForm invoiceForm = new InvoiceForm();
