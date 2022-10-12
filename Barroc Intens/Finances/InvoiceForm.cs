78a62e32aab432f068b4ae9fa436ba4f8098bc67
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2022-10-12 11:37:02 +0200

    added inputvalidation to invoiceform

--- Barroc Intens/Finances/InvoiceForm.cs
+++ Barroc Intens/Finances/InvoiceForm.cs
@@ -12,9 +12,51 @@ namespace Barroc_Intens.Finances
 {
     public partial class InvoiceForm : Form
     {
+        string _companyName;
+        string _companyAdress;
+        string _comment;
+        string _date;
+        decimal _hoursWorked;
+        decimal _discount;
+        decimal _pricePerHour;
+
         public InvoiceForm()
         {
             InitializeComponent();
+        }
+
+        private void btnCreateInvoice_Click(object sender, EventArgs e)
+        {
+          
+            _comment = txbComment.Text;
+            _date = dtpDate.Text.ToString();
+            _hoursWorked = nudHoursWorked.Value;
+            _discount = nudDiscount.Value;
+            _pricePerHour = nudHourlyPrice.Value;
+
+            if (stringInputValidation(txbCompanyName.Text) && stringInputValidation(txbCompanyAdress.Text) && stringInputValidation(_date))
+            {
+                //do stuff
+            }
+        }
+
+        private bool stringInputValidation(string companyInformation)
+        {
+            if (companyInformation == null)
+            {
+                lblError.Text = "Zorg ervoor dat alle velden zijn ingevuld";
+                return false;
+            }
+            return true;
+        }
+
+        private bool decimalInputValidation(float invoiceInformation)
+        {
+            if (invoiceInformation == 0)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
