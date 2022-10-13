4dce3577a041523ec4ff6a87614116fda529455c
stefano <72141994+D261324@users.noreply.github.com>
2022-10-13 21:01:54 +0200

    update custemer notes: design finished
    the design of prio  items finished

--- Barroc Intens/Sales/CustomerNotesForm.Designer.cs
+++ Barroc Intens/Sales/CustomerNotesForm.Designer.cs
@@ -29,34 +29,274 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnBackToMain = new System.Windows.Forms.Button();
+            this.txtConversationReport = new System.Windows.Forms.TextBox();
+            this.lblConversationReport = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.lblMenu = new System.Windows.Forms.Label();
+            this.lblInvoervelden = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblTitleHead = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lblActor = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Barroc_Intens.Properties.Resources.Logo6_klein;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 11);
+            this.pictureBox1.Location = new System.Drawing.Point(31, 13);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(131, 101);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
+            // btnBackToMain
+            // 
+            this.btnBackToMain.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBackToMain.Location = new System.Drawing.Point(1034, 12);
+            this.btnBackToMain.Name = "btnBackToMain";
+            this.btnBackToMain.Size = new System.Drawing.Size(112, 52);
+            this.btnBackToMain.TabIndex = 3;
+            this.btnBackToMain.Text = "Terug naar hoofd pagina";
+            this.btnBackToMain.UseVisualStyleBackColor = true;
+            // 
+            // txtConversationReport
+            // 
+            this.txtConversationReport.Location = new System.Drawing.Point(31, 117);
+            this.txtConversationReport.Multiline = true;
+            this.txtConversationReport.Name = "txtConversationReport";
+            this.txtConversationReport.Size = new System.Drawing.Size(451, 316);
+            this.txtConversationReport.TabIndex = 4;
+            // 
+            // lblConversationReport
+            // 
+            this.lblConversationReport.AutoSize = true;
+            this.lblConversationReport.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConversationReport.Location = new System.Drawing.Point(147, 89);
+            this.lblConversationReport.Name = "lblConversationReport";
+            this.lblConversationReport.Size = new System.Drawing.Size(159, 25);
+            this.lblConversationReport.TabIndex = 5;
+            this.lblConversationReport.Text = "Gespreksverslag";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(488, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 31);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(488, 236);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 31);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(488, 181);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(170, 31);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(488, 288);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(170, 31);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // lblMenu
+            // 
+            this.lblMenu.AutoSize = true;
+            this.lblMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMenu.Location = new System.Drawing.Point(544, 89);
+            this.lblMenu.Name = "lblMenu";
+            this.lblMenu.Size = new System.Drawing.Size(62, 25);
+            this.lblMenu.TabIndex = 10;
+            this.lblMenu.Text = "Menu";
+            // 
+            // lblInvoervelden
+            // 
+            this.lblInvoervelden.AutoSize = true;
+            this.lblInvoervelden.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInvoervelden.Location = new System.Drawing.Point(852, 89);
+            this.lblInvoervelden.Name = "lblInvoervelden";
+            this.lblInvoervelden.Size = new System.Drawing.Size(124, 25);
+            this.lblInvoervelden.TabIndex = 12;
+            this.lblInvoervelden.Text = "Invoervelden";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox2.Location = new System.Drawing.Point(664, 117);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(436, 316);
+            this.textBox2.TabIndex = 11;
+            // 
+            // lblTitleHead
+            // 
+            this.lblTitleHead.AutoSize = true;
+            this.lblTitleHead.Location = new System.Drawing.Point(703, 184);
+            this.lblTitleHead.Name = "lblTitleHead";
+            this.lblTitleHead.Size = new System.Drawing.Size(57, 17);
+            this.lblTitleHead.TabIndex = 13;
+            this.lblTitleHead.Text = "titel kop";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(687, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 17);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "text invoer";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(714, 291);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "label4";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(766, 181);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(314, 22);
+            this.textBox1.TabIndex = 17;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(766, 211);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(314, 72);
+            this.textBox4.TabIndex = 19;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(766, 288);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(314, 81);
+            this.textBox5.TabIndex = 20;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(880, 396);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 21;
+            // 
+            // lblActor
+            // 
+            this.lblActor.AutoSize = true;
+            this.lblActor.Location = new System.Drawing.Point(714, 157);
+            this.lblActor.Name = "lblActor";
+            this.lblActor.Size = new System.Drawing.Size(41, 17);
+            this.lblActor.TabIndex = 22;
+            this.lblActor.Text = "Actor";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(766, 153);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 21);
+            this.checkBox1.TabIndex = 23;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(880, 153);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(98, 21);
+            this.checkBox2.TabIndex = 24;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
             // CustomerNotesForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1158, 589);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.lblActor);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblTitleHead);
+            this.Controls.Add(this.lblInvoervelden);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblMenu);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblConversationReport);
+            this.Controls.Add(this.txtConversationReport);
+            this.Controls.Add(this.btnBackToMain);
             this.Controls.Add(this.pictureBox1);
             this.Name = "CustomerNotesForm";
             this.Text = "CustomerNotesForm";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnBackToMain;
+        private System.Windows.Forms.TextBox txtConversationReport;
+        private System.Windows.Forms.Label lblConversationReport;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label lblMenu;
+        private System.Windows.Forms.Label lblInvoervelden;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblTitleHead;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label lblActor;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
     }
 }
