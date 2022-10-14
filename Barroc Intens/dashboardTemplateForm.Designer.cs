ab5b79ff08d78496b3a7aac454b4b9b480525aee
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2022-10-14 16:25:47 +0200

    Update dashboardTemplateForm

--- Barroc Intens/dashboardTemplateForm.Designer.cs
+++ Barroc Intens/dashboardTemplateForm.Designer.cs
@@ -29,76 +29,36 @@
         private void InitializeComponent()
         {
             this.pnlLeft = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.lblAccountName = new System.Windows.Forms.Label();
-            this.lblDepartment = new System.Windows.Forms.Label();
-            this.btnLogout = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.lblExtraInfo = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.txbExtraInfo = new System.Windows.Forms.TextBox();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.lblDepartment = new System.Windows.Forms.Label();
+            this.lblAccountName = new System.Windows.Forms.Label();
+            this.pboxLogo = new System.Windows.Forms.PictureBox();
+            this.pnlTop = new System.Windows.Forms.Panel();
+            this.btnLocation1 = new System.Windows.Forms.Button();
+            this.btnLocation2 = new System.Windows.Forms.Button();
+            this.btnLocation3 = new System.Windows.Forms.Button();
+            this.lblSystemName = new System.Windows.Forms.Label();
             this.pnlLeft.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxLogo)).BeginInit();
+            this.pnlTop.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlLeft
             // 
             this.pnlLeft.BackColor = System.Drawing.SystemColors.ControlLight;
             this.pnlLeft.Controls.Add(this.lblExtraInfo);
-            this.pnlLeft.Controls.Add(this.textBox1);
+            this.pnlLeft.Controls.Add(this.txbExtraInfo);
             this.pnlLeft.Controls.Add(this.btnLogout);
             this.pnlLeft.Controls.Add(this.lblDepartment);
             this.pnlLeft.Controls.Add(this.lblAccountName);
-            this.pnlLeft.Controls.Add(this.pictureBox1);
+            this.pnlLeft.Controls.Add(this.pboxLogo);
             this.pnlLeft.Dock = System.Windows.Forms.DockStyle.Left;
             this.pnlLeft.Location = new System.Drawing.Point(0, 0);
             this.pnlLeft.Name = "pnlLeft";
             this.pnlLeft.Size = new System.Drawing.Size(222, 600);
             this.pnlLeft.TabIndex = 0;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::Barroc_Intens.Properties.Resources.Logo6_klein;
-            this.pictureBox1.Location = new System.Drawing.Point(57, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            // 
-            // lblAccountName
-            // 
-            this.lblAccountName.AutoSize = true;
-            this.lblAccountName.Location = new System.Drawing.Point(53, 220);
-            this.lblAccountName.Name = "lblAccountName";
-            this.lblAccountName.Size = new System.Drawing.Size(107, 20);
-            this.lblAccountName.TabIndex = 2;
-            this.lblAccountName.Text = "Account Naam";
-            // 
-            // lblDepartment
-            // 
-            this.lblDepartment.AutoSize = true;
-            this.lblDepartment.Location = new System.Drawing.Point(53, 249);
-            this.lblDepartment.Name = "lblDepartment";
-            this.lblDepartment.Size = new System.Drawing.Size(66, 20);
-            this.lblDepartment.TabIndex = 3;
-            this.lblDepartment.Text = "Afdeling";
-            // 
-            // btnLogout
-            // 
-            this.btnLogout.Location = new System.Drawing.Point(24, 525);
-            this.btnLogout.Name = "btnLogout";
-            this.btnLogout.Size = new System.Drawing.Size(162, 50);
-            this.btnLogout.TabIndex = 4;
-            this.btnLogout.Text = "Uitloggen";
-            this.btnLogout.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(24, 345);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(162, 160);
-            this.textBox1.TabIndex = 5;
             // 
             // lblExtraInfo
             // 
@@ -109,20 +69,106 @@
             this.lblExtraInfo.TabIndex = 6;
             this.lblExtraInfo.Text = "Extra Informatie";
             // 
-            // panel1
+            // txbExtraInfo
             // 
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(222, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(692, 600);
-            this.panel1.TabIndex = 1;
+            this.txbExtraInfo.Location = new System.Drawing.Point(24, 345);
+            this.txbExtraInfo.Multiline = true;
+            this.txbExtraInfo.Name = "txbExtraInfo";
+            this.txbExtraInfo.Size = new System.Drawing.Size(162, 160);
+            this.txbExtraInfo.TabIndex = 5;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Location = new System.Drawing.Point(24, 525);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(162, 50);
+            this.btnLogout.TabIndex = 4;
+            this.btnLogout.Text = "Uitloggen";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            // 
+            // lblDepartment
+            // 
+            this.lblDepartment.AutoSize = true;
+            this.lblDepartment.Location = new System.Drawing.Point(53, 249);
+            this.lblDepartment.Name = "lblDepartment";
+            this.lblDepartment.Size = new System.Drawing.Size(66, 20);
+            this.lblDepartment.TabIndex = 3;
+            this.lblDepartment.Text = "Afdeling";
+            // 
+            // lblAccountName
+            // 
+            this.lblAccountName.AutoSize = true;
+            this.lblAccountName.Location = new System.Drawing.Point(53, 220);
+            this.lblAccountName.Name = "lblAccountName";
+            this.lblAccountName.Size = new System.Drawing.Size(107, 20);
+            this.lblAccountName.TabIndex = 2;
+            this.lblAccountName.Text = "Account Naam";
+            // 
+            // pboxLogo
+            // 
+            this.pboxLogo.Image = global::Barroc_Intens.Properties.Resources.Logo6_klein;
+            this.pboxLogo.Location = new System.Drawing.Point(61, 12);
+            this.pboxLogo.Name = "pboxLogo";
+            this.pboxLogo.Size = new System.Drawing.Size(100, 100);
+            this.pboxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pboxLogo.TabIndex = 1;
+            this.pboxLogo.TabStop = false;
+            // 
+            // pnlTop
+            // 
+            this.pnlTop.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pnlTop.Controls.Add(this.lblSystemName);
+            this.pnlTop.Controls.Add(this.btnLocation3);
+            this.pnlTop.Controls.Add(this.btnLocation2);
+            this.pnlTop.Controls.Add(this.btnLocation1);
+            this.pnlTop.Location = new System.Drawing.Point(222, 0);
+            this.pnlTop.Name = "pnlTop";
+            this.pnlTop.Size = new System.Drawing.Size(692, 157);
+            this.pnlTop.TabIndex = 1;
+            // 
+            // btnLocation1
+            // 
+            this.btnLocation1.Location = new System.Drawing.Point(52, 117);
+            this.btnLocation1.Name = "btnLocation1";
+            this.btnLocation1.Size = new System.Drawing.Size(172, 37);
+            this.btnLocation1.TabIndex = 0;
+            this.btnLocation1.Text = "Location 1";
+            this.btnLocation1.UseVisualStyleBackColor = true;
+            // 
+            // btnLocation2
+            // 
+            this.btnLocation2.Location = new System.Drawing.Point(280, 117);
+            this.btnLocation2.Name = "btnLocation2";
+            this.btnLocation2.Size = new System.Drawing.Size(172, 37);
+            this.btnLocation2.TabIndex = 1;
+            this.btnLocation2.Text = "Location 2";
+            this.btnLocation2.UseVisualStyleBackColor = true;
+            // 
+            // btnLocation3
+            // 
+            this.btnLocation3.Location = new System.Drawing.Point(496, 117);
+            this.btnLocation3.Name = "btnLocation3";
+            this.btnLocation3.Size = new System.Drawing.Size(172, 37);
+            this.btnLocation3.TabIndex = 2;
+            this.btnLocation3.Text = "Location 3";
+            this.btnLocation3.UseVisualStyleBackColor = true;
+            // 
+            // lblSystemName
+            // 
+            this.lblSystemName.AutoSize = true;
+            this.lblSystemName.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSystemName.Location = new System.Drawing.Point(240, 34);
+            this.lblSystemName.Name = "lblSystemName";
+            this.lblSystemName.Size = new System.Drawing.Size(211, 41);
+            this.lblSystemName.TabIndex = 3;
+            this.lblSystemName.Text = "Systeemnaam";
             // 
             // dashboardTemplateForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(914, 600);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnlTop);
             this.Controls.Add(this.pnlLeft);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
@@ -130,7 +176,9 @@
             this.Text = "dashboardTemplateForm";
             this.pnlLeft.ResumeLayout(false);
             this.pnlLeft.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxLogo)).EndInit();
+            this.pnlTop.ResumeLayout(false);
+            this.pnlTop.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -138,12 +186,16 @@
         #endregion
 
         private System.Windows.Forms.Panel pnlLeft;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pboxLogo;
         private System.Windows.Forms.Label lblAccountName;
         private System.Windows.Forms.Label lblExtraInfo;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txbExtraInfo;
         private System.Windows.Forms.Button btnLogout;
         private System.Windows.Forms.Label lblDepartment;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnlTop;
+        private System.Windows.Forms.Label lblSystemName;
+        private System.Windows.Forms.Button btnLocation3;
+        private System.Windows.Forms.Button btnLocation2;
+        private System.Windows.Forms.Button btnLocation1;
     }
 }
