f74aeddf44c761a99216afdeee851c4d763acee6
AlirizaSari <72546588+AlirizaSari@users.noreply.github.com>
2022-10-17 14:24:12 +0200

    Added Inkoop form

---
+++ Barroc Intens/Inkoop/DashboardInkoopForm.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Barroc_Intens
+{
+    partial class DashboardInkoopForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlLeft = new System.Windows.Forms.Panel();
+            this.lblExtraInfo = new System.Windows.Forms.Label();
+            this.txbExtraInfo = new System.Windows.Forms.TextBox();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.lblDepartment = new System.Windows.Forms.Label();
+            this.lblAccountName = new System.Windows.Forms.Label();
+            this.pboxLogo = new System.Windows.Forms.PictureBox();
+            this.pnlTop = new System.Windows.Forms.Panel();
+            this.lblInkoop = new System.Windows.Forms.Label();
+            this.btnBackToMain = new System.Windows.Forms.Button();
+            this.btnStock = new System.Windows.Forms.Button();
+            this.btnInkoop = new System.Windows.Forms.Button();
+            this.pnlLeft.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxLogo)).BeginInit();
+            this.pnlTop.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlLeft
+            // 
+            this.pnlLeft.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pnlLeft.Controls.Add(this.lblExtraInfo);
+            this.pnlLeft.Controls.Add(this.txbExtraInfo);
+            this.pnlLeft.Controls.Add(this.btnLogout);
+            this.pnlLeft.Controls.Add(this.lblDepartment);
+            this.pnlLeft.Controls.Add(this.lblAccountName);
+            this.pnlLeft.Controls.Add(this.pboxLogo);
+            this.pnlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlLeft.Location = new System.Drawing.Point(0, 0);
+            this.pnlLeft.Name = "pnlLeft";
+            this.pnlLeft.Size = new System.Drawing.Size(222, 600);
+            this.pnlLeft.TabIndex = 0;
+            // 
+            // lblExtraInfo
+            // 
+            this.lblExtraInfo.AutoSize = true;
+            this.lblExtraInfo.Location = new System.Drawing.Point(20, 322);
+            this.lblExtraInfo.Name = "lblExtraInfo";
+            this.lblExtraInfo.Size = new System.Drawing.Size(91, 15);
+            this.lblExtraInfo.TabIndex = 6;
+            this.lblExtraInfo.Text = "Extra Informatie";
+            // 
+            // txbExtraInfo
+            // 
+            this.txbExtraInfo.Location = new System.Drawing.Point(24, 345);
+            this.txbExtraInfo.Multiline = true;
+            this.txbExtraInfo.Name = "txbExtraInfo";
+            this.txbExtraInfo.Size = new System.Drawing.Size(162, 160);
+            this.txbExtraInfo.TabIndex = 5;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Location = new System.Drawing.Point(24, 525);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(162, 50);
+            this.btnLogout.TabIndex = 4;
+            this.btnLogout.Text = "Uitloggen";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            // 
+            // lblDepartment
+            // 
+            this.lblDepartment.AutoSize = true;
+            this.lblDepartment.Location = new System.Drawing.Point(53, 249);
+            this.lblDepartment.Name = "lblDepartment";
+            this.lblDepartment.Size = new System.Drawing.Size(52, 15);
+            this.lblDepartment.TabIndex = 3;
+            this.lblDepartment.Text = "Afdeling";
+            // 
+            // lblAccountName
+            // 
+            this.lblAccountName.AutoSize = true;
+            this.lblAccountName.Location = new System.Drawing.Point(53, 220);
+            this.lblAccountName.Name = "lblAccountName";
+            this.lblAccountName.Size = new System.Drawing.Size(87, 15);
+            this.lblAccountName.TabIndex = 2;
+            this.lblAccountName.Text = "Account Naam";
+            // 
+            // pboxLogo
+            // 
+            this.pboxLogo.Image = global::Barroc_Intens.Properties.Resources.Logo6_klein;
+            this.pboxLogo.Location = new System.Drawing.Point(61, 12);
+            this.pboxLogo.Name = "pboxLogo";
+            this.pboxLogo.Size = new System.Drawing.Size(100, 100);
+            this.pboxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pboxLogo.TabIndex = 1;
+            this.pboxLogo.TabStop = false;
+            // 
+            // pnlTop
+            // 
+            this.pnlTop.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pnlTop.Controls.Add(this.lblInkoop);
+            this.pnlTop.Controls.Add(this.btnBackToMain);
+            this.pnlTop.Controls.Add(this.btnStock);
+            this.pnlTop.Controls.Add(this.btnInkoop);
+            this.pnlTop.Location = new System.Drawing.Point(222, 0);
+            this.pnlTop.Name = "pnlTop";
+            this.pnlTop.Size = new System.Drawing.Size(692, 157);
+            this.pnlTop.TabIndex = 1;
+            // 
+            // lblInkoop
+            // 
+            this.lblInkoop.AutoSize = true;
+            this.lblInkoop.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInkoop.Location = new System.Drawing.Point(293, 30);
+            this.lblInkoop.Name = "lblInkoop";
+            this.lblInkoop.Size = new System.Drawing.Size(95, 32);
+            this.lblInkoop.TabIndex = 3;
+            this.lblInkoop.Text = "Inkoop";
+            // 
+            // btnBackToMain
+            // 
+            this.btnBackToMain.Location = new System.Drawing.Point(496, 117);
+            this.btnBackToMain.Name = "btnBackToMain";
+            this.btnBackToMain.Size = new System.Drawing.Size(172, 37);
+            this.btnBackToMain.TabIndex = 2;
+            this.btnBackToMain.Text = "Terug naar homepagina";
+            this.btnBackToMain.UseVisualStyleBackColor = true;
+            this.btnBackToMain.Click += new System.EventHandler(this.btnBackToMain_Click);
+            // 
+            // btnStock
+            // 
+            this.btnStock.Location = new System.Drawing.Point(280, 117);
+            this.btnStock.Name = "btnStock";
+            this.btnStock.Size = new System.Drawing.Size(172, 37);
+            this.btnStock.TabIndex = 1;
+            this.btnStock.Text = "Voorraad";
+            this.btnStock.UseVisualStyleBackColor = true;
+            // 
+            // btnInkoop
+            // 
+            this.btnInkoop.Location = new System.Drawing.Point(52, 117);
+            this.btnInkoop.Name = "btnInkoop";
+            this.btnInkoop.Size = new System.Drawing.Size(172, 37);
+            this.btnInkoop.TabIndex = 0;
+            this.btnInkoop.Text = "Inkopen";
+            this.btnInkoop.UseVisualStyleBackColor = true;
+            // 
+            // DashboardInkoopForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 600);
+            this.Controls.Add(this.pnlTop);
+            this.Controls.Add(this.pnlLeft);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "DashboardInkoopForm";
+            this.Text = "DashboardInkoopForm";
+            this.pnlLeft.ResumeLayout(false);
+            this.pnlLeft.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxLogo)).EndInit();
+            this.pnlTop.ResumeLayout(false);
+            this.pnlTop.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlLeft;
+        private System.Windows.Forms.PictureBox pboxLogo;
+        private System.Windows.Forms.Label lblAccountName;
+        private System.Windows.Forms.Label lblExtraInfo;
+        private System.Windows.Forms.TextBox txbExtraInfo;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Label lblDepartment;
+        private System.Windows.Forms.Panel pnlTop;
+        private System.Windows.Forms.Label lblInkoop;
+        private System.Windows.Forms.Button btnBackToMain;
+        private System.Windows.Forms.Button btnStock;
+        private System.Windows.Forms.Button btnInkoop;
+    }
+}
