2131a759c44d81093fb3e59874f45207f03716ee
stefano <72141994+D261324@users.noreply.github.com>
2022-10-29 18:41:01 +0200

    new class: leasecontract
    needed for bkr registrition

---
+++ Barroc Intens/Classes/LeaseContract.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barroc_Intens.Classes
+{
+    internal class Leasecontract
+    {
+        public int id { get; set; }
+        public int CompanyId { get; set; }
+        public int BkrChecked { get; set; }
+        public int UserId { get; set; }
+    }
+}
+
