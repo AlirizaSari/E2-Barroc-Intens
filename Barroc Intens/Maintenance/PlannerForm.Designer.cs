00c1783cf0a91573eeacf4e1c7fc006efee3f458
stefano <72141994+D261324@users.noreply.github.com>
2022-10-30 16:30:21 +0100

    cleared screen for new design

--- Barroc Intens/Maintenance/PlannerForm.Designer.cs
+++ Barroc Intens/Maintenance/PlannerForm.Designer.cs
@@ -28,22 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.mcaMalfunctionCalendar = new System.Windows.Forms.MonthCalendar();
             this.mySqlCommand1 = new MySql.Data.MySqlClient.MySqlCommand();
-            this.dgvMaintenanceInformation = new System.Windows.Forms.DataGridView();
-            this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Customer = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Problem = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvMaintenanceInformation)).BeginInit();
+            this.btnBackToMaintenance = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // mcaMalfunctionCalendar
-            // 
-            this.mcaMalfunctionCalendar.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mcaMalfunctionCalendar.Location = new System.Drawing.Point(253, 53);
-            this.mcaMalfunctionCalendar.Name = "mcaMalfunctionCalendar";
-            this.mcaMalfunctionCalendar.ShowWeekNumbers = true;
-            this.mcaMalfunctionCalendar.TabIndex = 0;
             // 
             // mySqlCommand1
             // 
@@ -52,63 +39,31 @@
             this.mySqlCommand1.Transaction = null;
             this.mySqlCommand1.UpdatedRowSource = System.Data.UpdateRowSource.None;
             // 
-            // dgvMaintenanceInformation
+            // btnBackToMaintenance
             // 
-            this.dgvMaintenanceInformation.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvMaintenanceInformation.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Date,
-            this.Customer,
-            this.Problem});
-            this.dgvMaintenanceInformation.Location = new System.Drawing.Point(72, 272);
-            this.dgvMaintenanceInformation.Name = "dgvMaintenanceInformation";
-            this.dgvMaintenanceInformation.RowHeadersWidth = 51;
-            this.dgvMaintenanceInformation.RowTemplate.Height = 24;
-            this.dgvMaintenanceInformation.Size = new System.Drawing.Size(592, 150);
-            this.dgvMaintenanceInformation.TabIndex = 1;
-            // 
-            // Date
-            // 
-            this.Date.HeaderText = "Date";
-            this.Date.MinimumWidth = 6;
-            this.Date.Name = "Date";
-            this.Date.Width = 125;
-            // 
-            // Customer
-            // 
-            this.Customer.HeaderText = "Customer";
-            this.Customer.MinimumWidth = 6;
-            this.Customer.Name = "Customer";
-            this.Customer.Width = 125;
-            // 
-            // Problem
-            // 
-            this.Problem.HeaderText = "Problem";
-            this.Problem.MinimumWidth = 6;
-            this.Problem.Name = "Problem";
-            this.Problem.Width = 125;
+            this.btnBackToMaintenance.Location = new System.Drawing.Point(1489, 12);
+            this.btnBackToMaintenance.Name = "btnBackToMaintenance";
+            this.btnBackToMaintenance.Size = new System.Drawing.Size(168, 30);
+            this.btnBackToMaintenance.TabIndex = 3;
+            this.btnBackToMaintenance.Text = "Terug naar Dashboard";
+            this.btnBackToMaintenance.UseVisualStyleBackColor = true;
             // 
             // PlannerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dgvMaintenanceInformation);
-            this.Controls.Add(this.mcaMalfunctionCalendar);
+            this.ClientSize = new System.Drawing.Size(1669, 706);
+            this.Controls.Add(this.btnBackToMaintenance);
             this.Name = "PlannerForm";
             this.Text = "Agenda";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.PlannerForm_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dgvMaintenanceInformation)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.MonthCalendar mcaMalfunctionCalendar;
         private MySql.Data.MySqlClient.MySqlCommand mySqlCommand1;
-        private System.Windows.Forms.DataGridView dgvMaintenanceInformation;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Date;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Customer;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Problem;
+        private System.Windows.Forms.Button btnBackToMaintenance;
     }
 }
