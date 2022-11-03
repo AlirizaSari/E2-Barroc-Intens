9e929d6ce75ab4010795761f7a86348ecbd853c7
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2022-11-03 15:18:11 +0100

    Added invoiceOverviewForm

---
+++ Barroc Intens/Finances/InvoiceOverviewForm.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Barroc_Intens
+{
+    partial class InvoiceOverviewForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBackToFinanceDashboard = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBackToFinanceDashboard
+            // 
+            this.btnBackToFinanceDashboard.Location = new System.Drawing.Point(644, 12);
+            this.btnBackToFinanceDashboard.Name = "btnBackToFinanceDashboard";
+            this.btnBackToFinanceDashboard.Size = new System.Drawing.Size(144, 56);
+            this.btnBackToFinanceDashboard.TabIndex = 0;
+            this.btnBackToFinanceDashboard.Text = "Terug naar finance dashboard";
+            this.btnBackToFinanceDashboard.UseVisualStyleBackColor = true;
+            this.btnBackToFinanceDashboard.Click += new System.EventHandler(this.btnBackToFinanceDashboard_Click);
+            // 
+            // InvoiceOverviewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBackToFinanceDashboard);
+            this.Name = "InvoiceOverviewForm";
+            this.Text = "InvoiceOverviewForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBackToFinanceDashboard;
+    }
+}
---
+++ Barroc Intens/Finances/InvoiceOverviewForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Barroc_Intens
+{
+    public partial class InvoiceOverviewForm : Form
+    {
+        public InvoiceOverviewForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBackToFinanceDashboard_Click(object sender, EventArgs e)
+        {
+            DirectToForm(new DashboardFinanceForm());
+        }
+
+        private void DirectToForm(Form myForm)
+        {
+            this.Hide();
+            myForm.ShowDialog();
+            this.Close();
+        }
+    }
+}
