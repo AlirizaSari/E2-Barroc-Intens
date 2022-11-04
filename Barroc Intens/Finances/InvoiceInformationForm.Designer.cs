6e627663cd93946ee5aa68eccdaa39e4b77afe08
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2022-11-04 09:47:20 +0100

    Finished design of InvoiceInformation Form

--- Barroc Intens/Finances/InvoiceInformationForm.Designer.cs
+++ Barroc Intens/Finances/InvoiceInformationForm.Designer.cs
@@ -29,7 +29,27 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblCompany = new System.Windows.Forms.Label();
+            this.lblCompanyEmail = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblInvoicePaid = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txbCompany = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txbCompanyAddress = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txbNotes = new System.Windows.Forms.TextBox();
+            this.nudHoursWorked = new System.Windows.Forms.NumericUpDown();
+            this.nudPricePerHour = new System.Windows.Forms.NumericUpDown();
+            this.nudDiscount = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBackToInvoiceOverview = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHoursWorked)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPricePerHour)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDiscount)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -42,21 +62,197 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // InvoiceInformation
+            // lblCompany
+            // 
+            this.lblCompany.AutoSize = true;
+            this.lblCompany.Location = new System.Drawing.Point(37, 149);
+            this.lblCompany.Name = "lblCompany";
+            this.lblCompany.Size = new System.Drawing.Size(45, 16);
+            this.lblCompany.TabIndex = 1;
+            this.lblCompany.Text = "Bedrijf";
+            // 
+            // lblCompanyEmail
+            // 
+            this.lblCompanyEmail.AutoSize = true;
+            this.lblCompanyEmail.Location = new System.Drawing.Point(37, 177);
+            this.lblCompanyEmail.Name = "lblCompanyEmail";
+            this.lblCompanyEmail.Size = new System.Drawing.Size(85, 16);
+            this.lblCompanyEmail.TabIndex = 2;
+            this.lblCompanyEmail.Text = "Bedrijfsemail";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Bedrijfsadres";
+            // 
+            // lblInvoicePaid
+            // 
+            this.lblInvoicePaid.AutoSize = true;
+            this.lblInvoicePaid.Location = new System.Drawing.Point(38, 233);
+            this.lblInvoicePaid.Name = "lblInvoicePaid";
+            this.lblInvoicePaid.Size = new System.Drawing.Size(100, 16);
+            this.lblInvoicePaid.TabIndex = 4;
+            this.lblInvoicePaid.Text = "Factuur betaald";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(401, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Uren gewerkt";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(401, 177);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 16);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Korting";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(400, 205);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 16);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Prijs per uur";
+            // 
+            // txbCompany
+            // 
+            this.txbCompany.Location = new System.Drawing.Point(144, 146);
+            this.txbCompany.Name = "txbCompany";
+            this.txbCompany.Size = new System.Drawing.Size(162, 22);
+            this.txbCompany.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(144, 174);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(162, 22);
+            this.textBox2.TabIndex = 9;
+            // 
+            // txbCompanyAddress
+            // 
+            this.txbCompanyAddress.Location = new System.Drawing.Point(144, 202);
+            this.txbCompanyAddress.Name = "txbCompanyAddress";
+            this.txbCompanyAddress.Size = new System.Drawing.Size(162, 22);
+            this.txbCompanyAddress.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(144, 230);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 22);
+            this.textBox1.TabIndex = 11;
+            // 
+            // txbNotes
+            // 
+            this.txbNotes.Location = new System.Drawing.Point(507, 231);
+            this.txbNotes.Multiline = true;
+            this.txbNotes.Name = "txbNotes";
+            this.txbNotes.Size = new System.Drawing.Size(162, 76);
+            this.txbNotes.TabIndex = 15;
+            // 
+            // nudHoursWorked
+            // 
+            this.nudHoursWorked.Location = new System.Drawing.Point(507, 147);
+            this.nudHoursWorked.Name = "nudHoursWorked";
+            this.nudHoursWorked.Size = new System.Drawing.Size(162, 22);
+            this.nudHoursWorked.TabIndex = 17;
+            // 
+            // nudPricePerHour
+            // 
+            this.nudPricePerHour.Location = new System.Drawing.Point(507, 203);
+            this.nudPricePerHour.Name = "nudPricePerHour";
+            this.nudPricePerHour.Size = new System.Drawing.Size(162, 22);
+            this.nudPricePerHour.TabIndex = 18;
+            // 
+            // nudDiscount
+            // 
+            this.nudDiscount.Location = new System.Drawing.Point(507, 175);
+            this.nudDiscount.Name = "nudDiscount";
+            this.nudDiscount.Size = new System.Drawing.Size(162, 22);
+            this.nudDiscount.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(401, 234);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 16);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Notities";
+            // 
+            // btnBackToInvoiceOverview
+            // 
+            this.btnBackToInvoiceOverview.Location = new System.Drawing.Point(547, 12);
+            this.btnBackToInvoiceOverview.Name = "btnBackToInvoiceOverview";
+            this.btnBackToInvoiceOverview.Size = new System.Drawing.Size(122, 48);
+            this.btnBackToInvoiceOverview.TabIndex = 21;
+            this.btnBackToInvoiceOverview.Text = "Terug naar factuuroverzicht";
+            this.btnBackToInvoiceOverview.UseVisualStyleBackColor = true;
+            // 
+            // InvoiceInformationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(683, 319);
+            this.Controls.Add(this.btnBackToInvoiceOverview);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudDiscount);
+            this.Controls.Add(this.nudPricePerHour);
+            this.Controls.Add(this.nudHoursWorked);
+            this.Controls.Add(this.txbNotes);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txbCompanyAddress);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txbCompany);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblInvoicePaid);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblCompanyEmail);
+            this.Controls.Add(this.lblCompany);
             this.Controls.Add(this.pictureBox1);
-            this.Name = "InvoiceInformation";
+            this.Name = "InvoiceInformationForm";
             this.Text = "InvoiceInformation";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHoursWorked)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPricePerHour)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDiscount)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblCompany;
+        private System.Windows.Forms.Label lblCompanyEmail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblInvoicePaid;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txbCompany;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txbCompanyAddress;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txbNotes;
+        private System.Windows.Forms.NumericUpDown nudHoursWorked;
+        private System.Windows.Forms.NumericUpDown nudPricePerHour;
+        private System.Windows.Forms.NumericUpDown nudDiscount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBackToInvoiceOverview;
     }
 }
