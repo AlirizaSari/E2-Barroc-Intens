5159eada633f0ce5f6ca05ddf460210da6a914c6
AlirizaSari <72546588+AlirizaSari@users.noreply.github.com>
2022-11-04 09:38:29 +0100

    Changed convention addproductform

--- Barroc Intens/Inkoop/AddProductForm.cs
+++ Barroc Intens/Inkoop/AddProductForm.cs
@@ -38,7 +38,7 @@ namespace Barroc_Intens.Inkoop
                 Name = txbNameProduct.Text,
                 Description = txbDescriptionProduct.Text,
                 Brand = txbBrandProduct.Text,
-                CategoryId = (int)newProductCategoryComboBox.SelectedValue
+                CategoryId = (int)cboxNewProductCategory.SelectedValue
             };
             dbContext.Products.Add(product);
             dbContext.SaveChanges();
