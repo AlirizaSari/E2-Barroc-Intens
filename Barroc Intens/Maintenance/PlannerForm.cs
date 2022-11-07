725260151fe5e408691cfe747dcc0eb2e44dc761
stefano <72141994+D261324@users.noreply.github.com>
2022-11-07 13:43:34 +0100

    update code for appointments

--- Barroc Intens/Maintenance/PlannerForm.cs
+++ Barroc Intens/Maintenance/PlannerForm.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Barroc_Intens.Classes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Exchange.WebServices.Data;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,23 +16,19 @@ namespace Barroc_Intens.Maintenance
     public partial class PlannerForm : Form
     {
         private AppDbContext dbContext;
+        private MaintenanceAppointment _appointment;
 
         public PlannerForm()
         {
-            InitializeComponent();
-
-            
+            InitializeComponent();            
         }
 
         private void PlannerForm_Load(object sender, EventArgs e)
         {
             this.dbContext = new AppDbContext();
             this.dbContext.MaintenanceAppointments.Include(ma => ma.Company).Load();
-            this.maintenanceAppointmentBindingSource.DataSource = dbContext.MaintenanceAppointments.Local.ToBindingList();
-
-            
-            //var maintenanceAppointmentsCount = dbContext.MaintenanceAppointments.Where(ma => ma.appointmentFinished == true).Count();
-            
+            this.maintenanceAppointmentBindingSource.DataSource = dbContext.MaintenanceAppointments.Local.ToBindingList();            
+            lblCurrentNumberOfOpenTickets.Text = dbContext.MaintenanceAppointments.Where(ma => ma.AppointmentIsPlanned == true).Count().ToString();
         }
 
         private void btnBackToMaintenance_Click(object sender, EventArgs e)
@@ -47,10 +45,10 @@ namespace Barroc_Intens.Maintenance
         private void dataGridView1_SelectionChanged(object sender, EventArgs e)
         {
             //show the remark of the current selected appointment
-            var selectedAppointment = (MaintenanceAppointment)dataGridView1.CurrentRow.DataBoundItem;
-            txbCommandsAppointment.Text = selectedAppointment.Remark;
+            _appointment = (MaintenanceAppointment)dgvNewAppointmentsx.CurrentRow.DataBoundItem;
+            txbCommandsAppointment.Text = _appointment.Remark;
             //show the location of the current selected appointment
-            txbCompanyLocation.Text = selectedAppointment.Company.City;
+            txbCompanyLocation.Text = _appointment.Company.City;
 
         }
 
@@ -60,7 +58,42 @@ namespace Barroc_Intens.Maintenance
             var selectedDate = mcMaintanence.SelectionStart;
             var selectedAppointments = dbContext.MaintenanceAppointments.Where(ma => ma.AppointmentDate == selectedDate).ToList();
             dgvPlannedAppointments.DataSource = selectedAppointments;
+            // change the font weight of the selected date
+            mcMaintanence.BoldedDates = new DateTime[] { selectedDate };
+            // show the amount of appointments on the selected date
+            var selectedAppointmentsCount = dbContext.MaintenanceAppointments.Where(ma => ma.AppointmentDate == selectedDate).Count();
+            lblSelectedDate.Text = selectedDate.ToString("dd-MM-yyyy");
+            lblCurrentNumberOfOpenTickets.Text = selectedAppointmentsCount.ToString();   
             
+
+
+        }
+
+        private void btnCreateAppointment_Click(object sender, EventArgs e)
+        {
+            //select the current appointment selected in 
+            var selectedAppointment = (MaintenanceAppointment)dgvNewAppointmentsx.CurrentRow.DataBoundItem;
+
+            //change the appointment date to the selected date in the month calendar
+            selectedAppointment.AppointmentDate = mcMaintanence.SelectionStart;
+            //change the appointment time to the selected time in the time picker
+            selectedAppointment.AppointmentTime = dtpAppointmentTimeSet.Value;
+            //change the appointment employee to the selected employee 
+            selectedAppointment.AssignedEmployee = UserLoginInformation.LoginUserName;
+            //change the appointment duration to the selected duration in the textbox
+            selectedAppointment.AppointmentDuration = txbVisitDuration.Text;
+            //change the appointment finished to true
+            selectedAppointment.AppointmentIsPlanned = true;
+           
+            // the code below will save and refresh the database
+
+            dbContext.SaveChanges();
+            this.dgvNewAppointmentsx.Refresh();
+
+        }
+
+        private void txbCommandsAppointment_TextChanged(object sender, EventArgs e)
+        {
 
         }
     }
