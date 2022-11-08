cc78232b77256372f3a675ebe5022040d3043557
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2022-11-08 11:44:15 +0100

    Added form for CreateLeaseContract

---
+++ Barroc Intens/Finances/LeaseContracts/CreateLeaseContractForm.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Barroc_Intens.Finances.LeaseContracts
+{
+    public partial class CreateLeaseContractForm : Form
+    {
+        private AppDbContext dbContext;
+        string _companyName;
+
+        public CreateLeaseContractForm(string companyName)
+        {
+            InitializeComponent();
+            _companyName = companyName;        }
+
+        private void CreateInvoiceForm_Load(object sender, EventArgs e)
+        {
+            this.dbContext = new AppDbContext();
+            this.dbContext.Companies.Load();
+            this.dbContext.Products.Load();
+            this.companyBindingSource.DataSource = dbContext.Companies.Local.ToBindingList();
+            this.productBindingSource.DataSource = dbContext.Products.Local.ToBindingList();
+        }
+    }
+}
