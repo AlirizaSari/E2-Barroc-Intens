1101cfec6acdc7af9a0b9a4ed6c43c2884a7b783
AlirizaSari <72546588+AlirizaSari@users.noreply.github.com>
2022-11-11 16:47:18 +0100

    Added to sales form add company

---
+++ Barroc Intens/Sales/AddCompanyForm.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Barroc_Intens.Sales
+{
+    partial class AddCompanyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddCompanyForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txbCompanyName = new System.Windows.Forms.TextBox();
+            this.txbCompanyPhone = new System.Windows.Forms.TextBox();
+            this.txbCompanyEmail = new System.Windows.Forms.TextBox();
+            this.txbCompanyStreet = new System.Windows.Forms.TextBox();
+            this.txbCompanyHouseNumber = new System.Windows.Forms.TextBox();
+            this.txbCompanyCity = new System.Windows.Forms.TextBox();
+            this.txbCompanyCountryCode = new System.Windows.Forms.TextBox();
+            this.btnSaveCompany = new System.Windows.Forms.Button();
+            this.btnCancelCompany = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bedrijf Toevoegen";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Naam:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Telefoonnummer:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Email:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 163);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Straat:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(317, 70);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Huisnummer:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(317, 101);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(33, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Stad:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(317, 132);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Landcode:";
+            // 
+            // txbCompanyName
+            // 
+            this.txbCompanyName.Location = new System.Drawing.Point(130, 67);
+            this.txbCompanyName.Name = "txbCompanyName";
+            this.txbCompanyName.Size = new System.Drawing.Size(181, 22);
+            this.txbCompanyName.TabIndex = 8;
+            // 
+            // txbCompanyPhone
+            // 
+            this.txbCompanyPhone.Location = new System.Drawing.Point(130, 98);
+            this.txbCompanyPhone.Name = "txbCompanyPhone";
+            this.txbCompanyPhone.Size = new System.Drawing.Size(181, 22);
+            this.txbCompanyPhone.TabIndex = 9;
+            // 
+            // txbCompanyEmail
+            // 
+            this.txbCompanyEmail.Location = new System.Drawing.Point(130, 129);
+            this.txbCompanyEmail.Name = "txbCompanyEmail";
+            this.txbCompanyEmail.Size = new System.Drawing.Size(181, 22);
+            this.txbCompanyEmail.TabIndex = 10;
+            // 
+            // txbCompanyStreet
+            // 
+            this.txbCompanyStreet.Location = new System.Drawing.Point(130, 160);
+            this.txbCompanyStreet.Name = "txbCompanyStreet";
+            this.txbCompanyStreet.Size = new System.Drawing.Size(181, 22);
+            this.txbCompanyStreet.TabIndex = 11;
+            // 
+            // txbCompanyHouseNumber
+            // 
+            this.txbCompanyHouseNumber.Location = new System.Drawing.Point(405, 67);
+            this.txbCompanyHouseNumber.Name = "txbCompanyHouseNumber";
+            this.txbCompanyHouseNumber.Size = new System.Drawing.Size(181, 22);
+            this.txbCompanyHouseNumber.TabIndex = 12;
+            // 
+            // txbCompanyCity
+            // 
+            this.txbCompanyCity.Location = new System.Drawing.Point(405, 98);
+            this.txbCompanyCity.Name = "txbCompanyCity";
+            this.txbCompanyCity.Size = new System.Drawing.Size(181, 22);
+            this.txbCompanyCity.TabIndex = 13;
+            // 
+            // txbCompanyCountryCode
+            // 
+            this.txbCompanyCountryCode.Location = new System.Drawing.Point(405, 129);
+            this.txbCompanyCountryCode.Name = "txbCompanyCountryCode";
+            this.txbCompanyCountryCode.Size = new System.Drawing.Size(181, 22);
+            this.txbCompanyCountryCode.TabIndex = 14;
+            // 
+            // btnSaveCompany
+            // 
+            this.btnSaveCompany.Location = new System.Drawing.Point(339, 163);
+            this.btnSaveCompany.Name = "btnSaveCompany";
+            this.btnSaveCompany.Size = new System.Drawing.Size(121, 38);
+            this.btnSaveCompany.TabIndex = 15;
+            this.btnSaveCompany.Text = "Opslaan";
+            this.btnSaveCompany.UseVisualStyleBackColor = true;
+            this.btnSaveCompany.Click += new System.EventHandler(this.btnSaveCompany_Click);
+            // 
+            // btnCancelCompany
+            // 
+            this.btnCancelCompany.Location = new System.Drawing.Point(466, 163);
+            this.btnCancelCompany.Name = "btnCancelCompany";
+            this.btnCancelCompany.Size = new System.Drawing.Size(121, 38);
+            this.btnCancelCompany.TabIndex = 16;
+            this.btnCancelCompany.Text = "Annuleren";
+            this.btnCancelCompany.UseVisualStyleBackColor = true;
+            this.btnCancelCompany.Click += new System.EventHandler(this.btnCancelCompany_Click);
+            // 
+            // AddCompanyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 226);
+            this.Controls.Add(this.btnCancelCompany);
+            this.Controls.Add(this.btnSaveCompany);
+            this.Controls.Add(this.txbCompanyCountryCode);
+            this.Controls.Add(this.txbCompanyCity);
+            this.Controls.Add(this.txbCompanyHouseNumber);
+            this.Controls.Add(this.txbCompanyStreet);
+            this.Controls.Add(this.txbCompanyEmail);
+            this.Controls.Add(this.txbCompanyPhone);
+            this.Controls.Add(this.txbCompanyName);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AddCompanyForm";
+            this.Text = "Bedrijf Toevoegen";
+            this.Load += new System.EventHandler(this.AddCompanyForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txbCompanyName;
+        private System.Windows.Forms.TextBox txbCompanyPhone;
+        private System.Windows.Forms.TextBox txbCompanyEmail;
+        private System.Windows.Forms.TextBox txbCompanyStreet;
+        private System.Windows.Forms.TextBox txbCompanyHouseNumber;
+        private System.Windows.Forms.TextBox txbCompanyCity;
+        private System.Windows.Forms.TextBox txbCompanyCountryCode;
+        private System.Windows.Forms.Button btnSaveCompany;
+        private System.Windows.Forms.Button btnCancelCompany;
+    }
+}
